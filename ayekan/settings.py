8b0eba8a2bca858495b42c08ba22b42902ac1580
Leo Monti <leomontigatti@gmail.com>
2021-10-25 21:25:42 -0300

    Added allowed hosts

--- ayekan/settings.py
+++ ayekan/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['AYEKAN_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.17.206.210']
 
 
 # Application definition
